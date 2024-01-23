9273ddc608486afd2e5f03154bd44e8d90c5d5cf
Arreyetta Bawak Augustine Besong <112652000+ARREYETTA14@users.noreply.github.com>
2024-01-23 12:53:06 +0100

    Delete 3_Operators directory

--- 3_Operators/2_Identity.py
+++
@@ -1,4 +0,0 @@
-# a = "hello"
-# b = "hello"
-
-# print(a is not b)  # True, because a and b are different objects with different memory locations
--- 3_Operators/3_comparison.py
+++
@@ -1,8 +0,0 @@
-# account_name = "james"
-# account_number = "15448411891"
-# account_balance = 100000
-
-# if account_number == account_number:
-#     print (f"your balance is: ${account_balance}")
-
-
