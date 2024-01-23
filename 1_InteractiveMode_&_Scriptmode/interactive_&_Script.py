9c4dc148709a4fa237c3121ce7348c0c5d037752
Arreyetta Bawak Augustine Besong <112652000+ARREYETTA14@users.noreply.github.com>
2024-01-23 12:52:48 +0100

    Delete 1_InteractiveMode_&_Scriptmode directory

--- 1_InteractiveMode_&_Scriptmode/interactive_&_Script.py
+++
@@ -1,9 +0,0 @@
-## Interactive mode:
-# - Just to test for a function or varaiation in syntax
-
-
-## Script Mode
-
-# - Just using a py file to run python commands
-# - It is always just so logical.
-
